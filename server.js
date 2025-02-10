e22e46fd252b5ef9b5f7733bf7444691fec112e6
Gabriel <gehughes28@gmail.com>
2025-02-10 13:16:10 +0100

    fixed port

--- server.js
+++ server.js
@@ -172,7 +172,8 @@ app.get('/uploads-data', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
